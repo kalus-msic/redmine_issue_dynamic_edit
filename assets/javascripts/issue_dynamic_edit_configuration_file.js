80a72240738eb9b81143a701e6c676d0a0a3a799
Michael Kalus <michael.kalus@ms-ic.cz>
2026-08-20 00:27:41 +0200

    MSIC: double-click trigger + in-flow description editor
    - config: _CONF_LISTENER_TYPE_VALUE = dblclick (open edit on double-click)
    - css: render description inline editor in normal flow instead of a floating
      popup card; attribute dropdowns keep their compact popover look

--- assets/javascripts/issue_dynamic_edit_configuration_file.js
+++ assets/javascripts/issue_dynamic_edit_configuration_file.js
@@ -23,7 +23,7 @@ var _CONF_DISPLAY_EDIT_ICON = "single";
  * Choose which action will trigger the apparition of the edition block
  * Allowed values : click (default), dblclick, none
  */
-var _CONF_LISTENER_TYPE_VALUE = "click";
+var _CONF_LISTENER_TYPE_VALUE = "dblclick";
 
 /*
  * _CONF_LISTENER_TYPE_ICON (string)
